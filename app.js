7fe47631cf627e650cdbc634bf104a03843ace47
alain-quemat <alain.quemat@gmail.com>
2013-11-14 01:03:51 +0100

    update

--- app.js
+++ app.js
@@ -38,13 +38,7 @@ type: "POST"
 },
 
      getDeviceInfo = function () {
-        var element = document.getElementById('deviceProperties');
-        element.innerHTML = 'Device Name: '     + device.name     + '<br />' +
-                            'Device Cordova: '  + device.cordova  + '<br />' +
-                            'Device Platform: ' + device.platform + '<br />' +
-                            'Device UUID: '     + device.uuid     + '<br />' +
-                            'Device Model: '    + device.model    + '<br />' +
-                            'Device Version: '  + device.version  + '<br />';
+        alert('Device uuid: ' + device.uuid);
     };
 
     $('.camera-btn').on('click', takePicture);
